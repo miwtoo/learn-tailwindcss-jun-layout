01d602cc582d680f3759c0efc452980a73abbcc8
Miwtoo <miwyugioh@gmail.com>
2024-12-08 10:50:19 +0700

    feat: :sparkles: dashboard with collapsible sidebar

--- src/app/workshop/dashboard/page.tsx
+++ src/app/workshop/dashboard/page.tsx
@@ -1,5 +1,42 @@
+"use client";
 import { menuGroups } from "@/app/dashboard/_data/menu";
+import { triggerEdgeCollapse, triggerEdgeDrawer } from "tailwindcss-jun-layout";
 
 export default function WorkshopDashboard() {
-  return <div>Edit me...</div>;
+	return (
+		<div className="jun-layout">
+			<header className="jun-header">
+				<button
+					type="button"
+					className="jun-edgeDrawerTrigger"
+					onClick={() => triggerEdgeDrawer()}
+				>
+					Toggle
+				</button>
+				<button
+					type="button"
+					className="jun-collapsibleTrigger"
+					onClick={(event) => triggerEdgeCollapse({ event })}
+				>
+					Collapse
+				</button>
+				Header
+			</header>
+			<aside
+				className="
+				jun-edgeSidebar
+				jun-edgeSidebar-w-[256px]
+				jun-edgeSidebar-drawer
+				sm:jun-edgeSidebar-permanent
+				sm:jun-edgeSidebar-collapsed-w-[80px]
+				sm:jun-edgeSidebar-permanent-hoverUncollapse
+        		jun-edgeSidebar-permanent-autoCollapse-lg
+				"
+			>
+				<div className="jun-edgeContent">Sidebar Content</div>
+			</aside>
+			<main className="jun-content">Content</main>
+			<footer className="jun-footer">Footer</footer>
+		</div>
+	);
 }
