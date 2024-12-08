c06de0bedb6e405aa7a507d15377ba2ef923bd99
Miwtoo <miwyugioh@gmail.com>
2024-12-08 11:27:09 +0700

    feat: :sparkles: update dashboard layout

--- src/app/workshop/dashboard/page.tsx
+++ src/app/workshop/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { menuGroups } from "@/app/dashboard/_data/menu";
+import { MoreHorizontal } from "lucide-react";
 import { triggerEdgeCollapse, triggerEdgeDrawer } from "tailwindcss-jun-layout";
 
 export default function WorkshopDashboard() {
@@ -25,15 +26,41 @@ export default function WorkshopDashboard() {
 			<aside
 				className="
 				jun-edgeSidebar
-				jun-edgeSidebar-w-[256px]
-				jun-edgeSidebar-drawer
-				sm:jun-edgeSidebar-permanent
-				sm:jun-edgeSidebar-collapsed-w-[80px]
-				sm:jun-edgeSidebar-permanent-hoverUncollapse
+				jun-edgeSidebar-w-[200px]
+				jun-edgeSidebar-permanent
+				jun-edgeSidebar-collapsed-w-[48px]
+				jun-edgeSidebar-permanent-hoverUncollapse
         		jun-edgeSidebar-permanent-autoCollapse-lg
 				"
 			>
-				<div className="jun-edgeContent">Sidebar Content</div>
+				<div className="jun-edgeContent">
+					<div className="jun-sidebarContainer">
+						{menuGroups.map((group) => (
+							<div key={group.label} className="jun-sidebarGroup">
+								<div className="jun-sidebarGroupLabel">{group.label}</div>
+								<ul className="jun-sidebarMenu">
+									{group.items.map((menu) => {
+										const Icon = menu.icon;
+										return (
+											<li key={menu.label} className="jun-sidebarMenuItem">
+												<button type="button" className="jun-sidebarMenuButton">
+													<Icon className="jun-sidebarIcon" />
+													<span className="jun-sidebarText">{menu.label}</span>
+												</button>
+												<button
+													type="button"
+													className="jun-sidebarMenuAction jun-sidebarMenuAction-hoverAppear"
+												>
+													<MoreHorizontal />
+												</button>
+											</li>
+										);
+									})}
+								</ul>
+							</div>
+						))}
+					</div>
+				</div>
 			</aside>
 			<main className="jun-content">Content</main>
 			<footer className="jun-footer">Footer</footer>
